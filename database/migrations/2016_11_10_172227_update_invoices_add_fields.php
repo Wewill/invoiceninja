f0ee53f56cef795f54cea1c19895e238fac1b12c
yvoitenko <yvoitenko@justcoded.co>
2016-11-14 16:15:24 +0200

    removed unneccessarry spaces, replaced them with tabs

--- database/migrations/2016_11_10_172227_update_invoices_add_fields.php
+++ database/migrations/2016_11_10_172227_update_invoices_add_fields.php
@@ -6,31 +6,31 @@ use Illuminate\Support\Facades\Schema;
 
 class UpdateInvoicesAddFields extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-	    Schema::table('invoices', function(Blueprint $table){
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('invoices', function(Blueprint $table){
 		   $table->string('order_from')->nullable();
 		   $table->string('title')->nullable();
 		   $table->string('reference')->nullable();
-	    });
-    }
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-	    Schema::table('invoices', function(Blueprint $table){
-		    $table->dropColumn('order_from');
-		    $table->dropColumn('title');
-		    $table->dropColumn('reference');
-	    });
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('invoices', function(Blueprint $table){
+			$table->dropColumn('order_from');
+			$table->dropColumn('title');
+			$table->dropColumn('reference');
+		});
+	}
 }
